6faa262684b042607fcf35ad254d2a2ca2c9af63
themis1986 <themis_1986@hotmail.com>
2022-08-08 14:55:22 +0300

    Added styles

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -25,13 +25,36 @@ export default {
 <style scoped>
 .post {
   max-width: 1200px;
-  margin: 0 auto;
+  margin: 0 40px 30px;
+  padding-bottom: 30px;
+  border-bottom: 1px dashed #e7e7e7;
 }
 
 .post p {
   color: #444;
   line-height: 1.5rem;
   margin-top: 40px;
+}
+
+.post h3 {
+  display: inline-block;
+  position: relative;
+  font-size: 26px;
+  color: #fff;
+  margin-bottom: 10px;
+  max-width: 400px;
+}
+.post h3::before {
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #ff8800;
+  position: absolute;
+  z-index: -1;
+  padding-right: 40px;
+  left: -30px;
+  transform: rotateZ(-1deg);
 }
 
 .pre {
