a9533c830f71ba07e940c2d92c4a0698fa86d2f1
themis1986 <themis_1986@hotmail.com>
2022-08-08 12:44:40 +0300

    working with async code in the setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">Toggle posts</button>
-    <button @click="posts.pop()">Delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
@@ -15,22 +17,37 @@ export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: "welcome to the blog",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat pretium risus, eu gravida massa laoreet quis. Fusce vitae diam at neque congue congue. Etiam massa ligula, blandit quis mattis in, vestibulum eu quam. Suspendisse potenti. Aliquam rhoncus diam sit amet imperdiet facilisis. Sed quis augue sit amet erat rhoncus aliquet. Nulla non ante a eros vestibulum aliquet nec ut orci. Vestibulum felis magna, ultrices at nibh a, aliquam finibus ipsum. Duis eleifend sem pellentesque, ultrices tellus vitae, sodales risus.",
-        id: 1,
-      },
-      {
-        title: "top 5 CSS tips",
-        body: "Donec ac turpis eros. Aliquam vitae orci vitae dui finibus commodo. Duis quis rhoncus augue, in molestie lectus. Suspendisse auctor nibh turpis, eget consectetur purus vehicula in. Quisque aliquam consequat sem, quis blandit nulla hendrerit eget. Fusce ex ante, scelerisque eget nulla sed, dapibus tincidunt erat. Aenean vitae porttitor odio.",
-        id: 2,
-      },
-    ]);
+    // const posts = ref([
+    //   {
+    //     title: "welcome to the blog",
+    //     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat pretium risus, eu gravida massa laoreet quis. Fusce vitae diam at neque congue congue. Etiam massa ligula, blandit quis mattis in, vestibulum eu quam. Suspendisse potenti. Aliquam rhoncus diam sit amet imperdiet facilisis. Sed quis augue sit amet erat rhoncus aliquet. Nulla non ante a eros vestibulum aliquet nec ut orci. Vestibulum felis magna, ultrices at nibh a, aliquam finibus ipsum. Duis eleifend sem pellentesque, ultrices tellus vitae, sodales risus.",
+    //     id: 1,
+    //   },
+    //   {
+    //     title: "top 5 CSS tips",
+    //     body: "Donec ac turpis eros. Aliquam vitae orci vitae dui finibus commodo. Duis quis rhoncus augue, in molestie lectus. Suspendisse auctor nibh turpis, eget consectetur purus vehicula in. Quisque aliquam consequat sem, quis blandit nulla hendrerit eget. Fusce ex ante, scelerisque eget nulla sed, dapibus tincidunt erat. Aenean vitae porttitor odio.",
+    //     id: 2,
+    //   },
+    // ]);
 
-    const showPosts = ref(true);
+    const posts = ref([]);
+    const error = ref(null);
 
-    return { posts, showPosts };
+    const load = async () => {
+      try {
+        let response = await fetch("http://localhost:3000/posts");
+        if (!response.ok) {
+          throw Error("No data available");
+        }
+        posts.value = await response.json();
+      } catch (e) {
+        error.value = e.message;
+        console.log(error.value);
+      }
+    };
+    load();
+
+    return { posts, error };
   },
 };
 </script>
