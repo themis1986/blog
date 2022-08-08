395023d1838aa6edfadb8f60039aeba2b4c5636b
themis1986 <themis_1986@hotmail.com>
2022-08-08 11:29:22 +0300

    refs and reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinja">Update</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, reactive } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  setup() {
+    const ninjaOne = ref({ name: "mario", age: 30 });
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
+    const updateNinja = () => {
+      ninjaOne.value.age = 40;
+    };
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 40;
+    };
+
+    return { ninjaOne, ninjaTwo, updateNinja, updateNinjaTwo };
+  },
+};
 </script>
