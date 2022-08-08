5eeb09e15e8d06e2af0bac4f09b9001c9b142994
themis1986 <themis_1986@hotmail.com>
2022-08-08 11:51:15 +0300

    watch and watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,12 @@
     <input type="text" v-model="search" />
     <p>Search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="stopWatching">Stop Warching</button>
   </div>
 </template>
 
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, watch, watchEffect } from "vue";
 
 export default {
   name: "Home",
@@ -25,11 +26,28 @@ export default {
     ]);
     const search = ref("");
 
+    const stopWatch = watch(search, () => {
+      console.log("watch function ran");
+      if (search.value === "fuck") {
+        alert("WATCH YOUR TONGUE MATE");
+        search.value = "";
+      }
+    });
+
+    const stopEffect = watchEffect(() => {
+      console.log("watchEffect function ran", search.value);
+    });
+
+    const stopWatching = () => {
+      stopWatch();
+      stopEffect();
+    };
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value));
     });
 
-    return { names, search, matchingNames };
+    return { names, search, matchingNames, stopWatching };
   },
 };
 </script>
