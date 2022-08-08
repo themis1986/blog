477678d76b80519ea90e5e4b8b76ffe65790c37c
themis1986 <themis_1986@hotmail.com>
2022-08-08 18:22:35 +0300

    added createdAt property using firebase timestamp and ordering collections by timestamp

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -7,7 +7,10 @@ const getPosts = () => {
 
   const load = async () => {
     try {
-      const response = await projectFirestore.collection("posts").get();
+      const response = await projectFirestore
+        .collection("posts")
+        .orderBy("createdAt", "desc")
+        .get();
       posts.value = response.docs.map((doc) => {
         return { ...doc.data(), id: doc.id };
       });
