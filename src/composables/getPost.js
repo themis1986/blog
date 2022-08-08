0c42b1f08df66812cddd23a1718d118803419d6c
themis1986 <themis_1986@hotmail.com>
2022-08-08 13:15:27 +0300

    create simgle post fetching composable

---
+++ src/composables/getPost.js
@@ -0,0 +1,23 @@
+import { ref } from "vue";
+
+const getPost = (id) => {
+  const post = ref(null);
+  const error = ref(null);
+
+  const load = async () => {
+    try {
+      let response = await fetch(`http://localhost:3000/posts/${id}`);
+      if (!response.ok) {
+        throw Error("That post does not exist");
+      }
+      post.value = await response.json();
+    } catch (e) {
+      error.value = e.message;
+      console.log(error.value);
+    }
+  };
+
+  return { post, error, load };
+};
+
+export default getPost;
