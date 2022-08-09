3dcaf312fc5a8b659bef4f6eff5dc64a0c623875
thiakovidis <thiakovidis@agroapps.gr>
2022-08-09 19:35:49 +0300

    small change in deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:themis1986/blog.git main:gh-pages
+git push -f git@github.com:themis1986/blog.git master:gh-pages
 cd -
